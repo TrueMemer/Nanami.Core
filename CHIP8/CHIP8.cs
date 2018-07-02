388c5634cbbe42604b20d3928c3e1c896e0f54bc
Alexander Memer <mkoaleksedos@gmail.com>
2018-07-02 17:46:27 +0300

    Graphics support (games like breakout or pong work)

--- CHIP8/CHIP8.cs
+++ CHIP8/CHIP8.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using SDL2;
+using OpenTK.Graphics.OpenGL;
 using System.Runtime.InteropServices;
 
 namespace Nanami.Core.CHIP8
@@ -30,12 +30,12 @@ namespace Nanami.Core.CHIP8
             public Action<UInt16> Func;
         }
 
-        private List<TableEntry> OpcodeTable;
+        private TableEntry[] OpcodeTable;
 
         private const int PixelSize = 16;
 
         // Память 4КБ
-        public byte[] Memory = new byte[4096];
+        public byte[] Memory;
 
         // Фреймбуффер
         public byte[] Framebuffer = new byte[64 * 32];
@@ -53,12 +53,15 @@ namespace Nanami.Core.CHIP8
 
         // Счетчик инструкций
         // Обычно программы начинаются на 0x512
-        public UInt16 PC = 0x512;
+        public UInt16 PC = 512;
 
         // Таймеры
         public byte DelayTimer = 0;
         public byte SoundTimer = 0;
 
+        public bool[] Keypad = new bool[16];
+
+#if SDL
         // Указатель на окно SDL
         public IntPtr SDLWindow = IntPtr.Zero;
 
@@ -67,26 +70,64 @@ namespace Nanami.Core.CHIP8
 
         // Указатель на сёрфейс
         public SDL.SDL_Surface SDLSurface;
+#endif
 
-        public byte[] Screen = new byte[64 * 32];
-        public int ScreenWidth, ScreenHeight = 0;
+        //public byte[] Screen = new byte[64 * 32];
+        //public int ScreenWidth, ScreenHeight = 0;
+
+        public bool[,] Screen = new bool[64, 32];
+
+        public bool[,] ClearBuffer = new bool[64, 32];
+
+        public bool NeedRedraw;
+
+        public bool IsInputExecuted;
+
+        public UInt16 LastInput;
 
         // temp?
         public bool Running;
 
         public CHIP8()
         {
+            Memory = new byte[4096];
+            // Load fontset into memory
+            for (int i = 0; i < FontSet.Length; ++i)
+            {
+                Memory[i] = FontSet[i];
+            }
+            InitOpcodes();
             VMThread = new Thread(new ThreadStart(RunVM));
         }
+
+        byte[] FontSet = new byte[80]
+        {
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+        };
 
         // Инициализация таблицы опкодов
         public void InitOpcodes()
         {
-            OpcodeTable = new List<TableEntry>
+            OpcodeTable = new TableEntry[]
             {
-                new TableEntry { Opcode = 0x0000, Mask = 0xF000, Func = Opcode0NNN },
                 new TableEntry { Opcode = 0x00E0, Mask = 0xFFFF, Func = Opcode00E0 },
                 new TableEntry { Opcode = 0x00EE, Mask = 0xFFFF, Func = Opcode00EE },
+                new TableEntry { Opcode = 0x0000, Mask = 0xF000, Func = Opcode0NNN },
                 new TableEntry { Opcode = 0x1000, Mask = 0xF000, Func = Opcode1NNN },
                 new TableEntry { Opcode = 0x2000, Mask = 0xF000, Func = Opcode2NNN },
                 new TableEntry { Opcode = 0x3000, Mask = 0xF000, Func = Opcode3XNN },
@@ -125,29 +166,59 @@ namespace Nanami.Core.CHIP8
         // Запуск ВМ
         public void RunVM()
         {
-            UInt32 ticks = SDL.SDL_GetTicks();
-
-            for (;;)
+            for (; ; )
             {
-                UInt32 now = SDL.SDL_GetTicks();
+                Step();
+            }
+        }
 
-                if (now - ticks > 16)
+        public void Step()
+        {
+            if (DelayTimer > 0)
+            {
+                DelayTimer--;
+            }
+
+            if (SoundTimer > 0)
+            {
+                SoundTimer--;
+                if (SoundTimer == 0)
                 {
-                    UInt32 d = now - ticks;
-                    UInt32 t = d / 16;
-
-                    DelayTimer = (byte)Math.Max(0, DelayTimer - (int)t);
-                    SoundTimer = (byte)Math.Max(0, DelayTimer - (int)t);
-
-                    ticks = now - d % 16;
+                    Console.Beep();
                 }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                // Execute instruction
+                UInt16 instruction = 0;
+
+                if (PC + 1 >= 4096)
+                {
+                    Console.WriteLine($"PC out of bounds ({PC})");
+                    return;
+                }
+
+                instruction = (UInt16)(Memory.ElementAt(PC) << 8); // Big Endian
+                instruction |= Memory[PC + 1];
+
+                foreach (var entry in OpcodeTable)
+                {
+                    if ((instruction & entry.Mask) == entry.Opcode)
+                    {
+                        entry.Func(instruction);
+                        break;
+                    }
+                }
+
+                PC += 2;
             }
         }
 
         // Сброс
         public void Reset()
         {
-            PC = 0x512;
+            PC = 512;
 
             // Предположительно, все регистры при старте процессора равны нулю
             for (int i = 0; i < 16; i++)
@@ -163,133 +234,73 @@ namespace Nanami.Core.CHIP8
                 Memory[i] = 0;
             }
         }
-
         /*
-         * Инициализация графики
-         * 
-         * Использованные ресурсы:
-         *  - https://github.com/Kuzirashi/sdl2-cs-basics
-        */
-        public bool InitGraphics()
-        {
-            // Инициализация модулей SDL
-            if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO | SDL.SDL_INIT_AUDIO | SDL.SDL_INIT_TIMER) != 0)
-            {
-                Console.WriteLine($"Failed to initialize SDL: {SDL.SDL_GetError()}");
-                return false;
-            }
-
-            // Создание окна
-            SDLWindow = SDL.SDL_CreateWindow("Nanami.CHIP-8", SDL.SDL_WINDOWPOS_CENTERED, SDL.SDL_WINDOWPOS_CENTERED, 64 * PixelSize, 32 * PixelSize, 0);
-            if (SDLWindow == IntPtr.Zero)
-            {
-                Console.WriteLine($"Failed to create SDL window: {SDL.SDL_GetError()}");
-                DestroyGraphics();
-                return false;
-            }
-
-            // Создание рендерера
-            SDLRenderer = SDL.SDL_CreateRenderer(SDLWindow, -1, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
-            if (SDLRenderer == IntPtr.Zero)
-            {
-                Console.WriteLine($"Failed to create SDL renderer: {SDL.SDL_GetError()}");
-                DestroyGraphics();
-                return false;
-            }
-
-            var s = SDL.SDL_GetWindowSurface(SDLWindow);
-            if (s == IntPtr.Zero)
-            {
-                Console.WriteLine($"Failed to get window surface: {SDL.SDL_GetError()}");
-                DestroyGraphics();
-                return false;
-            }
-            SDLSurface = Marshal.PtrToStructure<SDL.SDL_Surface>(s);
-
-            return true;
-        }
-
-        public void Wait()
-        {
-            Running = true;
-            // TODO: Создать отдельную функцию для ивентов SDL
-            while (Running) {
-                SDL.SDL_Event e;
-                while (SDL.SDL_PollEvent(out e) == 1)
-                {
-                    if (e.type == SDL.SDL_EventType.SDL_QUIT)
-                    {
-                        Running = false;
-                        break;
-                    }
-                }
-                SDL.SDL_UpdateWindowSurface(SDLWindow);
-            }
-        }
-
-        // Чистка SDL
-        public void DestroyGraphics()
-        {
-            SDL.SDL_DestroyRenderer(SDLRenderer);
-            SDL.SDL_DestroyWindow(SDLWindow);
-            SDL.SDL_Quit();
-        }
-
-        public bool ReadMemoryImage(string path)
-        {
-            FileStream image;
-
-            try
-            {
-                image = File.Open(path, FileMode.Open, FileAccess.Read);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Failed to load file: {path} ({e.Message})");
-#if DEBUG
-                Console.WriteLine(e.StackTrace);
-#endif
-                return false;
-            }
-
-            MemoryStream m = new MemoryStream();
-            image.CopyTo(m);
-
-            Memory = m.ToArray();
-
-            m.Close();
-            image.Close();
-
-            return true;
-
-        }
-
         public void DrawPixel(int x, int y, byte color)
         {
             for (int k = y * PixelSize; k < (y + 1) * PixelSize; k++)
             {
                 for (int f = x * PixelSize; f < (x + 1) * PixelSize; k++)
                 {
-                    Screen[(k * ScreenWidth + f) * 4 + 0] = color;
-                    Screen[(k * ScreenWidth + f) * 4 + 1] = color;
-                    Screen[(k * ScreenWidth + f) * 4 + 2] = color;
-                    Screen[(k * ScreenWidth + f) * 4 + 3] = color;
+                    GL.Begin(BeginMode.Quads);
+
+                    GL.End();
                 }
             }
         }
+        */
 
         public void RedrawScreen()
         {
-            for (int y = 0; y < 32; y++)
+            for (int y = 0; y < 64; y++)
             {
-                for (int x = 0; y < 64; y++)
+                for (int x = 0; x < 32; x++)
                 {
-                    DrawPixel(x, y, (byte)(Framebuffer[x + y * 64] * 0xFF));
+                    if (Screen[y, x])
+                    {
+                        //DrawPixel(x, y, (byte)(Framebuffer[x + y * 64] * 0xFF));
+                        GL.Begin(BeginMode.Quads);
+                        GL.Vertex2(y * 10, x * 10);
+                        GL.Vertex2(y * 10, x * 10 + 10);
+                        GL.Vertex2((y + 1) * 10, x * 10 + 10);
+                        GL.Vertex2((y + 1) * 10, x * 10);
+                        GL.End();
+                        GL.Flush();
+                    }
                 }
             }
         }
 
-        #region Opcodes
+        public bool ReadMemoryImage(string path)
+        {
+
+            using (BinaryReader r = new BinaryReader(File.Open(path, FileMode.Open)))
+            {
+                for (int i = 0; i < r.BaseStream.Length; i++)
+                {
+                    Memory[512 + i] = r.ReadByte();
+                }
+            }
+
+            return true;
+
+        }
+
+        public void SetKey(UInt16 index, bool state)
+        {
+            Keypad[index] = state;
+
+            if (state)
+            {
+                IsInputExecuted = true;
+                LastInput = index;
+            }
+            else
+            {
+                IsInputExecuted = false;
+            }
+        }
+
+#region Opcodes
 
         private UInt16 GetNNN(UInt16 opcode)
         {
@@ -319,28 +330,23 @@ namespace Nanami.Core.CHIP8
         // Вызов программы (https://en.wikipedia.org/wiki/RCA_1802)
         private void Opcode0NNN(UInt16 opcode)
         {
-
         }
 
         // Очистка экрана
         private void Opcode00E0(UInt16 opcode)
         {
-            for (int i = 0; i < Framebuffer.Length; i++)
+            for (int i = 0; i < 64; i++)
             {
-                Framebuffer[i] = 0;
+                for (int j = 0; j < 32; j++)
+                {
+                    Screen[i, j] = false;
+                }
             }
-
-            RedrawScreen();
         }
 
         // Возвращает подпрограмму
         private void Opcode00EE(UInt16 opcode)
         {
-            if (CallStack.Count == 0)
-            {
-                // TODO:
-                return;
-            }
             PC = CallStack.Peek();
             CallStack.Pop();
         }
@@ -349,6 +355,9 @@ namespace Nanami.Core.CHIP8
         private void Opcode1NNN(UInt16 opcode)
         {
             PC = GetNNN(opcode);
+            PC -= 2;
+
+            //Console.WriteLine("NNN: {0}, Other: {0}", stuff, other);
         }
 
         // Вызов подпрограммы по адресу NNN
@@ -356,6 +365,7 @@ namespace Nanami.Core.CHIP8
         {
             CallStack.Push(PC);
             PC = GetNNN(opcode);
+            PC -= 2;
         }
 
         // Пропуск следующей инструкции если VX == NN
@@ -510,30 +520,77 @@ namespace Nanami.Core.CHIP8
         // Рисование спрайта
         private void OpcodeDXYN(UInt16 opcode)
         {
-            var x = V[GetX(opcode)] % 64; // ??
-            var y = V[GetY(opcode)] % 32; // ??
             var n = GetN(opcode);
-            bool flipped = false;
 
-            for (int j = 0; j < n; j++, y++)
+            for (var x = 0; x < 64; x++)
+            {
+                for (var y = 0; y < 32; y++)
+                {
+                    if (ClearBuffer[x, y])
+                    {
+                        if (Screen[x, y])
+                        {
+                            NeedRedraw = true;
+                        }
+
+                        ClearBuffer[x, y] = false;
+                        Screen[x, y] = false;
+                    }
+                }
+            }
+
+            V[0xF] = 0;
+
+            for (int j = 0; j < n; j++)
             {
                 var px = Memory[I + j];
                 for (int k = 0; k < 8; k++)
                 {
-                    var bit = (byte)((px >> k) & 1);
-                    if (bit == 1 && Framebuffer[x + k + y * 64] == 1)
+                    var x = (V[GetX(opcode)] + k) % 64; // ??
+                    var y = (V[GetY(opcode)] + j) % 32; // ??
+
+                    var spriteBit = ((px >> (7 - k)) & 1);
+                    var oldBit = Screen[x, y] ? 1 : 0;
+
+                    if (oldBit != spriteBit)
                     {
-                        flipped |= true;
+                        NeedRedraw = true;
                     }
-                    Framebuffer[x + k + y * 64] ^= bit;
+
+                    var newBit = oldBit ^ spriteBit;
+
+                    if (newBit != 0)
+                    {
+                        Screen[x, y] = true;
+                    }
+                    else
+                    {
+                        ClearBuffer[x, y] = true;
+                    }
+
+                    if (oldBit != 0 && newBit == 0)
+                    {
+                        V[0xF] = 1;
+                    }
                 }
             }
-
-            V[0xF] = Convert.ToByte(flipped);
         }
 
-        private void OpcodeEX9E(UInt16 opcode) {}
-        private void OpcodeEXA1(UInt16 opcode) {}
+        private void OpcodeEX9E(UInt16 opcode)
+        {
+            if (Keypad[V[GetX(opcode)]])
+            {
+                PC += 2;
+            }
+        }
+
+        private void OpcodeEXA1(UInt16 opcode)
+        {
+            if (!Keypad[V[GetX(opcode)]])
+            {
+                PC += 2;
+            }
+        }
 
         private void OpcodeFX07(UInt16 opcode)
         {
@@ -541,7 +598,17 @@ namespace Nanami.Core.CHIP8
             V[x] = DelayTimer;
         }
 
-        private void OpcodeFX0A(UInt16 opcode) {}
+        private void OpcodeFX0A(UInt16 opcode)
+        {
+            if (IsInputExecuted)
+            {
+                V[GetX(opcode)] = (byte)LastInput;
+            }
+            else
+            {
+                PC -= 2;
+            }
+        }
 
         private void OpcodeFX15(UInt16 opcode)
         {
@@ -555,12 +622,12 @@ namespace Nanami.Core.CHIP8
 
         private void OpcodeFX1E(UInt16 opcode)
         {
-            I = (byte)(I + V[GetX(opcode)]);
+            I = (UInt16)(I + V[GetX(opcode)]);
         }
 
         private void OpcodeFX29(UInt16 opcode)
         {
-
+            I = (UInt16)(V[GetX(opcode)] * 5);
         }
 
         private void OpcodeFX33(UInt16 opcode)
@@ -592,6 +659,6 @@ namespace Nanami.Core.CHIP8
             }
         }
 
-        #endregion
+#endregion
     }
 }
